9c8874909a0e5e934df328929f3686d5e47db944
Andrew <bachynckyi.andrii@gmail.com>
2022-06-29 13:10:53 +0300

    task_1+2

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const quantityCategory = document.querySelector('ul#categories').children;
+console.log(`Number of categories: ${quantityCategory.length}`);
+for(const element of quantityCategory){
+const nameCategory = element.querySelector('li.item h2').textContent;
+const quantityElements = element.querySelector('li.item  ul').children.length;
+console.log(`Category: ${nameCategory} \nElements: ${quantityElements}`);
+};
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,9 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const arr = ingredients.map(element => {
+  const li = document.createElement('li');
+  li.textContent = element;
+  return li;
+});
+document.querySelector('#ingredients').append(...arr);
