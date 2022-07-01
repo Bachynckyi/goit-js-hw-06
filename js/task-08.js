b8c61156c66b0aa6447ac3f31a8a34b8fff8266d
Andrew <bachynckyi.andrii@gmail.com>
2022-07-01 21:57:54 +0300

    task_8_9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit)
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        email,
+        password,
+    };
+    if (email === "" || password === "") {
+    return alert("Предупреждение! Все поля должны быть заполнены!");
+    }  
+    else {
+    console.log(email, password);
+    event.currentTarget.reset();
+    console.log(formData);
+    }
+    
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  buttonOn: document.querySelector('.change-color'),
+  bodyColor: document.querySelector('body'),
+  spanText: document.querySelector('.color')
+}
+
+
+
+refs.buttonOn.addEventListener('click', () => {
+  refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  refs.spanText.textContent = getRandomHexColor();
+});
+
+
